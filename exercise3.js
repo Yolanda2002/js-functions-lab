50c5b642be25bb1e8bcd81516a7684ac9d22d885
Jia Yang <20104736@mail.wit.ie>
2023-09-20 06:08:16 +0800

    Exercise 3.

---
+++ exercise3.js
@@ -0,0 +1,16 @@
+fetch("https://jsonplaceholder.typicode.com/todos")
+  .then(response => response.json())
+  .then(json => {
+     const uncompleted = json.filter(todo => todo.completed===false)
+     .map(todo =>{
+        return {
+            userId: todo.userId,
+            title: todo.title
+        }
+     }); 
+     console.log(uncompleted); // Show the array
+  })
+  .catch(function(err) { 
+    console.log(err);
+  });
+
